f1ff1989094f7c5792c828e91fcd08e44ec4274d
Bruce Kroeze <bruce@ecomsmith.com>
2018-10-04 20:45:56 -0700

    Added missing test file

---
+++ test/utils/parser.test.js
@@ -0,0 +1,117 @@
+import test from 'ava';
+import Result from 'folktale/result';
+import Parser from '../../src/utils/parser';
+import { ERRORS } from '../../src/models/constants';
+
+const parser = new Parser();
+
+test('Should split comments from token', (t) => {
+  const result = parser.tokenizeShortcut("1 ETH ;test");
+  t.deepEqual(result.getOrElse('error'), {
+    tokens: ['1', 'ETH'],
+    comment: 'test',
+  });
+});
+
+test('Should parse a simple trade', (t) => {
+  parser.parseTrade('1 ETH @ $100')
+    .matchWith({
+      Ok: ({value}) => t.deepEqual(value, {
+        debit: ['1', 'ETH'],
+        credit: ['100', 'USD'],
+        comment: null,
+        connector: '@',
+        reversed: false,
+      }),
+      Error: ({value}) => {
+        console.log('ERR', value);
+        t.fail(value)
+      },
+    });
+});
+
+test('Should parse out a comment', (t) => {
+  const result = parser.parseTrade('1 ETH = $100 ;foo');
+  t.deepEqual(result.getOrElse('error'), {
+    debit: ['1', 'ETH'],
+    credit: ['100', 'USD'],
+    comment: 'foo',
+    connector: '=',
+    reversed: false,
+  });  
+});
+
+test('Should parse out a negative entry trade', (t) => {
+  const result = parser.parseTrade('-1 ETH @ $100 bank ;foo');
+  t.deepEqual(result.getOrElse('error'), {
+    credit: ['1', 'ETH'],
+    debit: ['100', 'USD', 'bank'],
+    comment: 'foo',
+    connector: '@',
+    reversed: true,
+  });  
+});
+
+
+test('Should fail if too short', (t) => {
+  const result = parser.parseTrade('1');
+  t.is(result.getOrElse('error'), 'error');
+});
+
+test('Should fail if just a debit', (t) => {
+  parser.parseTrade('1 ETH bank')
+    .matchWith({
+      Ok: _ => t.fail('Should have not accepted without a second part of the trade.'),
+      Error: ({ value }) => t.true(value instanceof ERRORS.ParseErrors.InvalidTrade),
+    });
+});
+
+test('Should fail if no currency', (t) => {
+  parser.parseTrade('1 @ 100 USD')
+    .matchWith({
+      Ok: _ => t.fail('Should have not accepted without a currency.'),
+      Error: _ => t.pass('Should fail'),
+    });
+});
+
+test('Should fail with two numeric', (t) => {
+  parser.parseEntry('1 1 ;test')
+    .matchWith({
+      Ok: ({value}) => t.fail(JSON.stringify(value)),
+      Error: ({value}) => t.true(value instanceof ERRORS.ParseErrors.InvalidShortcut),
+    });
+});
+
+test('Should fail with two non-numeric', (t) => {
+  parser.parseEntry('x y')
+    .matchWith({
+      Ok: ({value}) => t.fail(JSON.stringify(value)),
+      Error: ({value}) => t.true(value instanceof ERRORS.ParseErrors.InvalidShortcut),
+    });
+});
+
+
+test('Should parse a debit', (t) => {
+  parser.parseEntry('1 ETH')
+    .matchWith({
+      Ok: ({value}) => t.deepEqual(value, {
+        entry: ['1', 'ETH'],
+        comment: null,
+      }),
+      Error: ({value}) => t.fail(value)
+    });
+});
+
+test('Should parse a debit with a comment and account', (t) => {
+  parser.parseEntry('1 ETH Test:Account ;foo')
+    .matchWith({
+      Ok: ({value}) => t.deepEqual(value, {
+        entry: ['1', 'ETH', 'Test:Account'],
+        comment: 'foo',
+      }),
+      Error: ({value}) => t.fail(value)
+    });
+});
+
+
+
